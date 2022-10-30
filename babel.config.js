ccc5ae55915cc103f001d299ba34bbd17c91d433
Turgut Kurt <turgut@softcand.com>
2022-10-30 21:30:09 +0300

    added absolute path & svg & vector icons

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,46 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'babel-plugin-root-import',
+      {
+        paths: [
+          {
+            rootPathPrefix: '~',
+            rootPathSuffix: 'src',
+          },
+          {
+            rootPathPrefix: '~assets',
+            rootPathSuffix: 'src/assets/index',
+          },
+          {
+            rootPathPrefix: '~components',
+            rootPathSuffix: 'src/components/index',
+          },
+          {
+            rootPathPrefix: '~config',
+            rootPathSuffix: 'src/config/index',
+          },
+          {
+            rootPathPrefix: '~hooks',
+            rootPathSuffix: 'src/hooks/index',
+          },
+          {
+            rootPathPrefix: '~navigation',
+            rootPathSuffix: 'src/navigation/index',
+          },
+          {
+            rootPathPrefix: '~screens',
+            rootPathSuffix: 'src/screens/index',
+          },
+          {
+            rootPathPrefix: '~utils',
+            rootPathSuffix: 'src/utils/index',
+          },
+        ],
+      },
+    ],
+  ],
 };
