15d8a089fb8402f7d6f0b7e44286a146fda255f3
Turgut Kurt <turgut@softcand.com>
2022-10-31 11:47:51 +0300

    deleted custom input

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,6 @@ export * from './config';
 // exports All components
 export * from './CustomText';
 export * from './CustomButton';
-export * from './CustomInputLabel';
 export * from './ProductList';
 export * from './MedProductCard';
 export * from './FavButton';
